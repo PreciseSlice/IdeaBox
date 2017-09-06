ff6d25a1038f5d4fb74c7e969e9974ea803af8f5
Valentino Valentin <valentinovalentine95@gmail.com>
2017-09-06 12:08:27 -0600

    delete button

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $('button').on('click', userEnterBtn);
-$('.delete').on('click', '.delete', userDeleteBtn);
+$('.bottom').on('click', '.delete', userDeleteBtn);
 
 // var quality = ["swill", "plausible", "plausible", "genius" ]
 // for(var i =0; i<quality.length; i++)
+
+function userDeleteBtn(e) {
+  e.preventDefault();
+  $(this).parents('.idea').remove() 
+}
 
 
 
@@ -13,11 +18,11 @@ function userEnterBtn(e){
 
   $('.bottom').prepend(
       `<article class='idea'>
-       <h2>${title}</h2> 
-       <p>${body}</p> <hr />
-       <button class='upvote'></button>
-       <button class='downvote'></button>
-       <button class='delete'></button>
+         <h2>${title}</h2> 
+         <p>${body}</p> <hr />
+         <button class='upvote'></button>
+         <button class='downvote'></button>
+         <button class='delete'></button>
       </article>`);
   $('.idea-input').val("");
   $('.body-input').val("");
